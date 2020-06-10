e1a37ddc24989b7d9815da144026d3eceacdcd23
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-10 11:25:06 -0700

    Add spec to js for if input is divisible by 100
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,9 @@ $(document).ready(function() {
 // Buisness logic
 
 function leapYear(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
   } else {
     return false;
